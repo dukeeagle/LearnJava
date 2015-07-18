f2b62853b43539301977a7af2b6c4dffb059230d
Luke Igel <luke.igel@gmail.com>
2015-07-18 11:40:47 -0500

    Adds PigDicegame

--- PigDice.java
+++ PigDice.java
@@ -1,4 +1,4 @@
-import public.util.Scanner;
+import java.util.Scanner;
 
 public class PigDice
 {
@@ -9,8 +9,8 @@ public class PigDice
 		int roll, total1, total2, turnTotal;
 		String choice = "";
 		
-		total1 = 0
-		total2 = 0
+		total1 = 0;
+		total2 = 0;
 		
 		do
 		{
@@ -19,7 +19,7 @@ public class PigDice
 			
 			do
 			{
-				roll1 = 1 + (int)(Math.random()*6);
+				roll = 1 + (int)(Math.random()*6);
 				System.out.println( "\tYou rolled a " + roll + "." );
 				if ( roll == 1 )
 				{
@@ -35,12 +35,49 @@ public class PigDice
 				}
 			} while ( roll != 1 && choice.equals("roll") );
 		
-			total += turnTotal;
-			System.out.println( "\tYou end the round with " + total1 + " points."
-		}
-		
-		
-		
+			total1 += turnTotal;
+			System.out.println( "\tYou end the round with " + total1 + " points.");
+			
+			if ( total1 < 100 )
+			{
+				turnTotal = 0;
+				System.out.println( "Computer has " + total2 + " points." );
+				
+				do
+				{
+					roll = 1 + (int)(Math.random()*6);
+					System.out.println( "\tComputer rolled a " + roll + " points." );
+					if ( roll == 1 )
+					{
+						System.out.println( "\tThat ends its turn.");
+						turnTotal = 0;
+					}
+					else
+					{
+						turnTotal += roll;
+						System.out.println( "\tComputer has " + turnTotal + " points so far this roun.");
+						
+						if ( turnTotal < 20 )
+						{
+							System.out.println( "\tComputer chooses to roll again.");
+						}
+					}
+				} while ( roll != 1 && turnTotal < 20 );
+				total2 += turnTotal;
+				System.out.println( "\tComputer ends the round with " + total2 + " points." );
+					
+			}
+		} while (total1 < 100 && total2 < 100);
+				
+				if ( total1 > total2 )
+				{
+					System.out.println( "Humanity wins!");
+				}
+				else
+				{
+					System.out.println( "The computer wins.");
+				}
+				
 			
 		
 	}
